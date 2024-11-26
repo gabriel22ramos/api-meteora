d50d46e76ecdd89ade2fe11b6311ca483195a26d
Gabriel Ramos <gabrielramoss232@gmail.com>
2024-11-26 16:32:31 -0300

    ter

---
+++ api/src/main/java/br/com/meteora/api/security/SecurityConfig.java
@@ -0,0 +1,35 @@
+package br.com.meteora.api.security;
+
+import org.springframework.context.annotation.*;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+    
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+         
+       http.authorizeHttpRequests( auth -> auth
+       .requestMatchers("/index","/sobre","/contato").permitAll()
+       .anyRequest().authenticated())
+        
+        .formLogin(form -> form
+        .loginPage("/login")
+        .defaultSuccessUrl("/", true)
+        .failureUrl("/login-error")
+        .permitAll())
+
+        .logout(logout -> logout
+        .logoutSuccessUrl("/"));
+        
+
+
+
+        
+        
+        
+        
+        return http.build();
+    }
+}
